3819ac7295079906ff1929bfbd98166e3e2ddd25
Ariya Ikhwanu <ariyaikhwanu@gmail.com>
2020-11-18 17:27:53 +0700

    Initial Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title', 'Belajar Laravel')
+    
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Hallo brooooo</h1>
+        </div>
+    </div>
+</div>
+@endsection
